95f4e2319a00327058404e699dbee58ff2ac3bb5
Efa-Iwa <gistend@gmail.com>
2022-03-15 06:43:59 -0700

    prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0..
+ */
+int main(void)
+{
+	puts("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+prints _putchar, followed by a new line
