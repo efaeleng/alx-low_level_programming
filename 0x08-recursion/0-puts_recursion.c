46ab578630c16423b72cac032a4151e42251a13a
Efa-Iwa <gistend@gmail.com>
2022-03-30 09:28:47 -0700

    function that prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,20 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints string in reverse
- * @s: pointer to string
- * *Return: void
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: pointer to the string
+ * Return: void
  */
 
-void _print_rev_recursion(char *s)
+void _puts_recursion(char *s)
 {
-	if (*s > '\0')
+	if (*s == '\0')
 	{
-		_print_rev_recursion(s + 1);
+		_putchar('\n');
+	}
+	else
+	{
 		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 }
