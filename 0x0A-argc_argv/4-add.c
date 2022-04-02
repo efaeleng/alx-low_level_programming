02e0808947e6b77ec83d102c9e9926a336e3806e
Efa-Iwa <gistend@gmail.com>
2022-04-01 18:10:20 -0700

    program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,48 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 #include "main.h"
 
 /**
- * isInteger - checks if s is an integer
- * @s: string to check
- * Return: 0 or 1
+ * main - Program that takes in all integer arguments and returns the sum
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
  */
 
-int isInteger(const char *s)
+int main(int argc, char *argv[])
 {
-	int i = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] < '0' || s[i] > '9')
-			return (1);
-		i++;
-	}
-	return (0);
-}
+	int i, j, length, sum;
+	char *ptr;
 
-/**
- * main - adds positive numbers
- * @argc: int
- * @argv: list
- * Return: 0
- */
-
-int main(int argc, char const *argv[])
-{
-	int sum = 0;
-	while (--argc)
+	if (argc < 2)
+		printf("0\n");
+	else
 	{
-		if (isInteger(argv[argc]))
+		sum = 0;
+		for (i = 1; i < argc; i++)
 		{
-			printf("Error\n");
-			return (1);
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for (j = 0; j < length; j++)
+			{
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(argv[i]);
 		}
-		sum += atoi(argv[argc]);
+
+		printf("%d\n", sum);
 	}
-
-	printf("%i\n", sum);
-
 	return (0);
 }
