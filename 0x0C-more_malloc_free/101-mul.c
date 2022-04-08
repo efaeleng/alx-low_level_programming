102dc03532cec5c21338f959e052d02237159e12
Efa-Iwa <gistend@gmail.com>
2022-04-08 09:26:09 -0700

    program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,204 +1,92 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _calloc - allocate (`size' * `nmemb') bytes and set to 0
- * @nmemb: number of elements
- * @size: number of bytes per element
- *
- * Return: pointer to memory, or NULL if `nmemb' or `size' is 0 or malloc fails
+ * _puts - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	unsigned int i;
-	char *p;
 
-	if (size == 0 || nmemb == 0)
-		return (NULL);
-	p = malloc(nmemb * size);
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; i < nmemb * size; ++i)
-		p[i] = 0;
-	return (p);
+
+void _puts(char *str)
+{
+	int i = 0;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
 }
 
 /**
- * _strdigit - check if string `s' is composed only of digits
- * @s: string to check
- *
- * Return: 1 if true, 0 if false
+ * _atoi - convert a string to an integer.
+ * @s: char type string
+ * Return: integer converted
  */
-int _strdigit(char *s)
+
+int _atoi(const char *s)
 {
-	if (*s == '-' || *s == '+')
-		++s;
-	while (*s)
+	int sign = 1;
+	unsigned long int resp = 0, firstNum, i;
+
+	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
 	{
-		if (*s < '0' || *s > '9')
+		if (s[firstNum] == '-')
 		{
-			return (0);
+			sign *= -1;
 		}
-		++s;
 	}
-	return (1);
-}
 
-/**
- * _puts - print string `s'
- * @s: string to print
- */
-void _puts(char *s)
-{
-	while (*s)
-		_putchar(*(s++));
-}
-
-/**
- * rev_num_str - reverse a number string up to trailing zeros
- * @start: beginning of number
- * @end: end of number
- * @ns: string containing number
- */
-void rev_num_str(int start, int end, char *ns)
-{
-	int i, j;
-	char tmp;
-
-	while (ns[end] == 0 && end != start)
-		--end;
-	for (i = start, j = end; i <= j; ++i, --j)
+	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
 	{
-		tmp = ns[i] + '0';
-		ns[i] = ns[j] + '0';
-		ns[j] = tmp;
+		resp *= 10;
+		resp += (s[i] - 48);
 	}
+
+	return (sign * resp);
 }
 
 /**
- * _strlen - calculate length of string `s'
- * @s: string to get length of
- *
- * Return: length of string
+ * print_int - prints an integer.
+ * @n: int
+ * Return: 0
  */
-int _strlen(char *s)
-{
-	int i;
 
-	for (i = 0; s[i]; ++i)
-				;
-	return (i);
+void print_int(unsigned long int n)
+{
+
+	unsigned  long int divisor = 1, i, resp;
+
+	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+		;
+
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
+	{
+		resp = n / divisor;
+		_putchar('0' + resp);
+	}
+
 }
 
 /**
- * strmul - multply two numbers as strings
- * @a: first number
- * @b: second number
- *
- * Return: pointer to result on success, or NULL on failure
+ * main - print the result of the multiplication, followed by a new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
  */
-char *strmul(char *a, char *b)
-{
-	int la, lb, i, j, k, l, neg = 0;
-	char *result;
-	char mul, mul_carry, sum, sum_carry;
 
-	if (*a == '-')
-	{
-		neg ^= 1;
-		++a;
-	}
-	if (*b == '-')
-	{
-		neg ^= 1;
-		++b;
-	}
-	la = _strlen(a);
-	lb = _strlen(b);
-	result = _calloc(la + lb + 1 + neg, sizeof(char));
-	if (result == NULL)
-		return (NULL);
-	if (neg)
-		result[0] = '-';
-	for (i = lb - 1, l = neg; i >= 0; --i, ++l)
-	{
-		mul_carry = 0;
-		sum_carry = 0;
-		for (j = la - 1, k = l; j >= 0; --j, ++k)
-		{
-			mul = (a[j] - '0') * (b[i] - '0') + mul_carry;
-			mul_carry = mul / 10;
-			mul %= 10;
-			sum = result[k] + mul + sum_carry;
-			sum_carry = sum / 10;
-			sum %= 10;
-			result[k] = sum;
-		}
-		result[k] = sum_carry + mul_carry;
-	}
-	rev_num_str(neg, k, result);
-	return (result);
-}
-
-/**
- * main - multiply two numbers from the command line and print the result
- * @argc: argument count
- * @argv: argument list
- *
- * Return: 0 if successful, 98 if failure
- */
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-	char *result;
+	(void)argc;
 
 	if (argc != 3)
 	{
-		_puts("Error\n");
+		_puts("Error ");
 		exit(98);
 	}
-	if (!_strdigit(argv[1]) || !_strdigit(argv[2]))
-	{
-		_puts("Error\n");
-		exit(98);
-	}
-	result = strmul(argv[1], argv[2]);
-	if (result == NULL)
-	{
-		_puts("Error\n");
-		exit(98);
-	}
-	_puts(result);
+	print_int(_atoi(argv[1]) * _atoi(argv[2]));
 	_putchar('\n');
-	free(result);
-	exit(EXIT_SUCCESS);
+
+	return (0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,7 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
 
 /**
  *  Auth: Efa
@@ -8,32 +10,13 @@
  */
 
 int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
-int _abs(int n);
-int _isupper(int c);
-int _isdigit(int c);
-int _strlen(char *s);
-void _puts(char *s);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-
-
-
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void _puts(char *str);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
 
 #endif
