742d8757c9557c5638699faf2b5547d676f4acf4
Efa-Iwa <gistend@gmail.com>
2022-04-07 18:41:54 -0700

    added protoypes...

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -13,8 +13,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void _puts(char *str);
-int _atoi(const char *s);
+int _strlen(char *s);
+int is_digit(char *s);
 void print_int(unsigned long int n);
+void errors(void);
 
 #endif
