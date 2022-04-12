03e53dabe399d9fe4478a3665be731ff46e1c491
Efa-Iwa <gistend@gmail.com>
2022-04-12 02:50:57 -0700

    first commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,32 @@
 #ifndef DOG_H
 #define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * struct dog - dog data
- * @name: name of the dog
- * @age: age of the dog
- * @owner: name of the owner of the dog
- *
- * Description: Defines metadata about the dog and type of dog's struct
+ * struct dog - attributes of a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the dog's owner
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
-/* Additional function prototypes */
-int _putchar(char c);
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+
+
+#endif /* DOG_H */
