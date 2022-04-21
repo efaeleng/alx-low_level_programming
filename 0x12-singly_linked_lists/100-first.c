415dad067a695592feab07b496300c6cc314d886
Efa-Iwa <gistend@gmail.com>
2022-04-21 14:24:27 -0700

    gets printed before the main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * premain - run before main
  */
-void __attribute__ ((constructor)) main()
+void __attribute__ ((constructor)) premain()
 {
 	printf(
 			"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
